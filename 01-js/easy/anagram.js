eb5e9ebb21c095b8ba4893d04b5a0ba8edeb8af7
Sanket Kharat <sanketkharat28@gmail.com>
2024-06-18 21:11:46 +0530

    solved anagram.js assignment from easy section of 01-js

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -6,6 +6,22 @@
 
 function isAnagram(str1, str2) {
 
+  
+  // Remove any non-alphanumeric characters and convert to lowercase
+  const cleanStr1 = str1.replace(/[^a-z0-9]/gi, '').toLowerCase();
+  const cleanStr2 = str2.replace(/[^a-z0-9]/gi, '').toLowerCase();
+
+  // IMPORTANT we are calculating whether str1 and str2's length is equal and not cleanStr1 and cleanStr2
+  if (str1.length !== str2.length) {
+    return false;
+  }
+
+  // Sort the characters of both cleaned strings and compare them
+  const sortedStr1 = cleanStr1.split('').sort().join('');
+  const sortedStr2 = cleanStr2.split('').sort().join('');
+
+  // If the sorted cleaned strings are equal, they are anagrams
+return sortedStr1 === sortedStr2
 }
 
 module.exports = isAnagram;
